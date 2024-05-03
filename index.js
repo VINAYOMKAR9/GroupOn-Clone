2579026b397fd669e36aa1e3a9652c2c5460cafc
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-05-03 22:20:34 +0530

    Groupon-Clone

---
+++ index.js
@@ -0,0 +1,382 @@
+
+let Data = [
+    { id: 1,
+        title:'Julie Massage',
+        discount: 'Up to 50% Off on Swedish Massage at Versailles Massage...',
+        address:'1136 West Armitage Avenue, Chicago•3.1 m',
+        price: '$85 From $40 52% OFF',
+        promo: '$14 with code PROMO',
+        description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+        bougth: '20+ bought',
+        image: 'https://img.grouponcdn.com/iam_raw/kchWfunDQfXaj1Q3PsHM/5a-5616x3744/v1/c349x211.webp',
+        rating: {
+            rate: 4.9,
+            count: 120
+        }},
+        { id: 2,
+            title:'Elysium SurgiSpa',
+            discount: 'Up to 50% Off on Thai Massage at MT Massage Therapy',
+            address:'329 West 18th Street, Chicago•1.7 mi',
+            price: '$80 From $40 50% OFF',
+            promo: '$30 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '220+ bought',
+        image: 'https://img.grouponcdn.com/deal/4Lu5GHAN2NXKKegVMjbLgEoe6jAf/4L-4879x3685/v1/c349x211.webp',
+        rating: {
+            rate: 4.9,
+            count: 320
+        }},
+        { id: 3,
+            title:'King Spa and Sauna',
+            discount: 'Up to 53% Off on Choice of Deep Tissue or Swedish Massa...',
+            address:'1136 West Armitage Avenue, Chicago•3.1 m',
+            price: '$85 From $40 52% OFF',
+            promo: '$20 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '210+ bought',
+            image: 'https://img.grouponcdn.com/deal/z8kRBLEKRvzUVtJQ48Qu14nSFuY/z8-1810x1090/v1/c349x211.webp',
+            rating: {
+                rate: 3.4,
+                count: 120
+            }},   
+        ];
+        
+        let Sdata =[{id: 9,
+            title:'Pure massage Spa',
+            discount: 'Gift the 90-Minute Deep-Tissue ultimate relaxation experience this Mother D...',
+            address:'809 Civic Center Dr., Niles•13.4 mi',
+            price: '$49 From $29 40% OFF',
+            promo: '$30 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '110790+ bought',
+            image: 'https://img.grouponcdn.com/deal/2d5wZBvoAz9Uh5QoM2SRPpZxtNmC/2d-5627x3752/v1/c349x211.webp',
+            rating: {
+                rate: 4.9,
+                count: 120
+            }},
+            {id: 10,
+                title:'Versailles Massage & Bar',
+                discount: 'Up to 50% Off on Swedish Massage at Versailles Massage..',
+                address:'809 Civic Center Dr., Niles•13.4 mi',
+                price: '$90 From $45 50% OFF',
+                description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+                promo: '$50 with code PROMO',
+                bougth: '110790+ bought',
+                image: 'https://img.grouponcdn.com/metro_draft_service/3GqzTohMv5de2sk3JPdDrQDqwiTj/3G-700x420/v1/c349x211.webp',
+        rating: {
+            rate: 3.9,
+            count: 100
+        }},
+        {id: 11,
+            title:'Pure massage Spa',
+            discount: 'Up to 37% Off on Nail Spa/Salon - Shellac / No-Chip / G..',
+            address:'809 Civic Center Dr., Niles•13.4 mi',
+            price: '$50 From $40 10% OFF',
+            promo: '$20 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '110790+ bought',
+            image: 'https://img.grouponcdn.com/deal/3GbfEGiBvo8PeCMcLBAxEzJSYrxV/3G-2048x1229/v1/c349x211.webp',
+            rating: {
+                rate: 4.1,
+                count: 160
+            }},
+            {id: 12,
+                title:'Mirror Mirror Spa Salon',
+                discount: 'Gift the 90-Minute Deep-Tissue ultimate relaxation experience this Mother D...',
+                address:'809 Civic Center Dr., Niles•13.4 mi',
+                price: '$102 From $88 30% OFF',
+                promo: '$24 with code PROMO',
+                description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+                bougth: '110790+ bought',
+                image: 'https://img.grouponcdn.com/deal/5wvzi9NCeoUcRNodQonZ/Y7-2048x1229/v1/c349x211.webp',
+                rating: {
+                    rate: 4.5,
+                    count: 110
+                }}
+            ];
+            
+            let Ldata=[{ id: 13,
+                title:'Julie Massage',
+                discount: 'Up to 50% Off on Swedish Massage at Versailles Massage...',
+                address:'1136 West Armitage Avenue, Chicago•3.1 m',
+                promo: '$41 with code PROMO',
+                price: '$85 From $40 52% OFF',
+                description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    bougth: '20+ bought',
+    image: 'https://img.grouponcdn.com/bynder/wZVTgtWqNLVXuW7dMgWrDCRpvum/wZ-2048x1229/v1/c349x211.webp',
+    rating: {
+        rate: 4.1,
+        count: 120
+    }},
+    { id: 14,
+        title:'Sam Club',
+        discount: 'One-Year Sam  Club Membership or Sam Club Plus Membe...',
+        address:'2601 Cicero Avenue, Cicero•6.6 mi',
+        price: '$50 From $14 72% OFF',
+        promo: '$31 with code PROMO',
+        description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+        bougth: '109790+ bought',
+        image: 'https://img.grouponcdn.com/deal/4P6uTgniAsQke1pbMN1Y6KWCdpqB/4P-960x582/v1/c349x211.webp',
+        rating: {
+            rate: 4.7,
+            count: 1200
+        }},
+        { id: 17,
+            title:'King Spa and Sauna',
+            discount: 'Gift the ultimate relaxation experience this Mother D...',
+            address:'809 Civic Center Dr., Niles•13.4 mi',
+            price: '$50 From $35 30% OFF',
+            promo: '$19 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '100+ bought',
+    image: 'https://img.grouponcdn.com/deal/ibjVHGNc2ismnc86Br6X/mN-3000x1800/v1/c349x211.webp',
+    rating: {
+        rate: 3.9,
+        count: 120
+    }},
+    { id: 18,
+        title:'Julie Massage',
+        discount: 'Up to 50% Off on Swedish Massage at Versailles Massage...',
+        address:'1136 West Armitage Avenue, Chicago•3.1 m',
+        price: '$85 From $40 52% OFF',
+        promo: '$29 with code PROMO',
+        description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+        bougth: '20+ bought',
+        image: 'https://img.grouponcdn.com/iam_raw/kchWfunDQfXaj1Q3PsHM/5a-5616x3744/v1/c349x211.webp',
+        rating: {
+            rate: 4.9,
+            count: 120
+        }},
+        { id: 19,
+            title:'Elysium SurgiSpa',
+            discount: 'Up to 50% Off on Thai Massage at MT Massage Therapy',
+            address:'329 West 18th Street, Chicago•1.7 mi',
+            price: '$80 From $40 50% OFF',
+            promo: '$20 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '220+ bought',
+            image: 'https://img.grouponcdn.com/deal/4Lu5GHAN2NXKKegVMjbLgEoe6jAf/4L-4879x3685/v1/c349x211.webp',
+            rating: {
+                rate: 4.9,
+                count: 320
+        }},
+        { id: 20,
+            title:'King Spa and Sauna',
+            discount: 'Up to 53% Off on Choice of Deep Tissue or Swedish Massa...',
+            address:'1136 West Armitage Avenue, Chicago•3.1 m',
+            price: '$85 From $40 52% OFF',
+            promo: '$30 with code PROMO',
+            description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+            bougth: '210+ bought',
+            image: 'https://img.grouponcdn.com/deal/z8kRBLEKRvzUVtJQ48Qu14nSFuY/z8-1810x1090/v1/c349x211.webp',
+            rating: {
+                rate: 3.4,
+                count: 120
+            }},   
+            { id: 21,
+                title:'Pure Serenity Spa',
+                discount: '60- or 90-Minute Deep-Tissue or Swedish Massage with Co...',
+                address:'1136 West Armitage Avenue, Chicago•3.1 ',
+                price: '$110 From $89 19% OFF',
+                promo: '$32 with code PROMO',
+                description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+                bougth: '1090+ bought',
+                image: 'https://img.grouponcdn.com/metro_draft_service/2JDvEciQwAh7XynEaA1FH95Ha3sv/2J-4032x3024/v1/c349x211.webp',
+                rating: {
+                    rate: 3.1,
+                    count: 120
+                }},
+                { id: 22,
+                    title:'Pure Spa',
+                    discount: 'Gift the 90-Minute Deep-Tissue ultimate relaxation experience this Mother D...',
+                    address:'809 Civic Center Dr., Niles•13.4 mi',
+                    price: '$49 From $29 40% OFF',
+                    promo: '$22 with code PROMO',
+                    description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+                    bougth: '110790+ bought',
+                    image: 'https://img.grouponcdn.com/deal/47Sz1xj4BUjqSdVoFRg8iMr5YpyV/47-1810x1090/v1/c349x211.webp',
+                    rating: {
+                        rate: 4.9,
+                        count: 120
+                    }},
+                ]
+                
+    appendData(Data);
+    function appendData(data){
+                    // document.querySelector("#container").innerHTML = null                
+    data.map((el,i)=>{     
+                        
+        let card = document.createElement('div');
+        let ratt = document.createElement('p');
+        let img = document.createElement('img');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let p6 = document.createElement('p');
+        p.setAttribute('id','para')
+        p2.setAttribute('id','address')
+        p3.setAttribute('id','price')
+        p4.setAttribute('id','promo') 
+        p5.setAttribute('id','title')
+        p6.setAttribute('id','bougth')
+        img.setAttribute('id','cimg')
+        ratt.setAttribute('id','ratt')
+        // let p.
+        let random = (Math.floor(Math.random()*100) +100) /10;
+             
+        img.src= el.image;
+        ratt.textContent=`${el.rating.rate} ⭐⭐⭐⭐⭐ | ${random}K Rating `
+        p.textContent= el.discount;
+        p2.textContent= el.address;
+        p3.textContent=el.price;
+        p4.textContent=el.promo;
+        p5.textContent=el.title;
+        p6.textContent=el.bougth;
+    
+        card.append(img,p,ratt,p2,p3,p4,p5,p6);
+        document.querySelector("#container").append(card);
+
+    })
+}
+appendsData(Sdata)
+function appendsData(sdata){
+    // document.querySelector("#container").innerHTML = null
+    sdata.map((el,i)=>{     
+                        
+        let card = document.createElement('div');
+        let ratt = document.createElement('p');
+        let img = document.createElement('img');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let p6 = document.createElement('p');
+        p.setAttribute('id','para')
+        p2.setAttribute('id','address')
+        p3.setAttribute('id','price')
+        p4.setAttribute('id','promo') 
+        p5.setAttribute('id','title')
+        p6.setAttribute('id','bougth')
+        img.setAttribute('id','cimg')
+        ratt.setAttribute('id','ratt')
+        // let p.
+        let random = (Math.floor(Math.random()*100) +100) /10;
+             
+        img.src= el.image;
+        ratt.textContent=`${el.rating.rate} ⭐⭐⭐⭐⭐ | ${random}K Rating `
+        p.textContent= el.discount;
+        p2.textContent= el.address;
+        p3.textContent=el.price;
+        p4.textContent=el.promo;
+        p5.textContent=el.title;
+        p6.textContent=el.bougth;
+    
+        card.append(img,p,ratt,p2,p3,p4,p5,p6);    
+        document.querySelector("#sContainer").append(card);
+
+    })
+}
+
+appendtData(Ldata)
+function appendtData(Ldata){
+    // document.querySelector("#container").innerHTML = null
+    Ldata.map((el,i)=>{     
+                        
+        let card = document.createElement('div');
+        let ratt = document.createElement('p');
+        let img = document.createElement('img');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let p6 = document.createElement('p');
+        p.setAttribute('id','para')
+        p2.setAttribute('id','address')
+        p3.setAttribute('id','price')
+        p4.setAttribute('id','promo') 
+        p5.setAttribute('id','title')
+        p6.setAttribute('id','bougth')
+        img.setAttribute('id','cimg')
+        ratt.setAttribute('id','ratt')
+        // let p.
+        let random = (Math.floor(Math.random()*100) +100) /10;
+             
+        img.src= el.image;
+        ratt.textContent=`${el.rating.rate} ⭐⭐⭐⭐⭐ | ${random}K Rating `
+        p.textContent= el.discount;
+        p2.textContent= el.address;
+        p3.textContent=el.price;
+        p4.textContent=el.promo;
+        p5.textContent=el.title;
+        p6.textContent=el.bougth;
+    
+        card.append(img,p,ratt,p2,p3,p4,p5,p6);    
+        document.querySelector("#tContainer").append(card);
+
+    })
+}
+
+
+
+let showMore=document.querySelector('#showMore');
+showMore.addEventListener('click',()=>{
+    appendData(Data);
+})
+let sshowMore=document.querySelector('#sshowMore');
+sshowMore.addEventListener('click',()=>{
+    appendsData(Sdata);
+})
+
+let downarrow = document.querySelector('#navdivLArrow');
+downarrow.addEventListener('click',()=>{
+    
+    
+})
+
+let fil= document.getElementById('filterr');
+  fil.addEventListener('change', ()=> {
+      let selectedValue = fil.value;
+      if(selectedValue=='https://www.groupon.com/travel'){
+          window.open(selectedValue, "_blank");
+        }
+        else if(selectedValue=='https://www.groupon.com/local?topcategory=local'){
+            window.open(selectedValue,'_blank')
+        }
+        else if(selectedValue=='https://www.groupon.com/goods'){
+            window.open(selectedValue,'_blank')
+        }     
+        else if(selectedValue=='https://www.groupon.com/coupons'){
+        window.open(selectedValue,'_blank')
+    }     
+});
+
+let country = document.querySelector('#countryId');
+country.addEventListener("change",()=>{
+    let countryValue =  country.value;
+    if(countryValue=='https://www.groupon.com/'){
+        window.open(countryValue, "_blank");
+    }
+    else if(countryValue=='https://www.groupon.fr/'){
+        window.open(countryValue,'_blank');
+    }
+    else if(countryValue=='https://www.groupon.ae/'){
+        window.open(countryValue,'_blank');
+    }
+    else if(countryValue=='https://www.groupon.ca/'){
+        window.open(countryValue,'_blank');
+    }
+})
+
+
+
+
+// console.log();
+// 1. firstly take tag by id ,class
+// 2. add addEventListener
+// 3. value leni he within the callBack function 
+
+
